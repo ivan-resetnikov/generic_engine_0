33e800262225935c4cecd2cb82e06599dc6ceb7d
ivan-resetnikov <work@ivan-reshetnikov.dev>
2025-08-24 22:05:52 +0200

    File ending in \n

--- editor/editor.c
+++ editor/editor.c
@@ -5,11 +5,23 @@
 
 #include "../core/core.h"
 
+void draw_surface_update();
+
+WindowContext g_window = {0};
+
 int main(int arg_count, char* arg_vec[])
 {
     LOG("editor");
 
-    
+    create_window(&g_window);
+    start_main_loop(&g_window, draw_surface_update);
+    close_window(&g_window);
 
     return 0;
+}
+
+void draw_surface_update()
+{
+    // Most likely, one display call per <refresh rate>
+    // 60Hz = 60 calls per second / 60 FPS
 }
--- runtime/runtime.c
+++ runtime/runtime.c
@@ -11,6 +11,8 @@ WindowContext g_window = {0};
 
 int main(int arg_count, char* arg_vec[])
 {
+    LOG("runtime");
+
     create_window(&g_window);
     start_main_loop(&g_window, draw_surface_update);
     close_window(&g_window);
