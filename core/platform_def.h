c484462384c779fc7639d30339c459090b98d25f
ivan-resetnikov <work@ivan-reshetnikov.dev>
2025-08-24 19:39:11 +0200

    platform_def.h encapsulated as #pragma once

--- core/platform_def.h
+++ core/platform_def.h
@@ -3,9 +3,14 @@
  * Copyright (c) 2025-present Ivan Reshetnikov, see license.txt
 */
 
+#ifndef PLATFORM_DEF_H
+#define PLATFORM_DEF_H
+
 #ifdef __linux__
     #define PLATFORM_LINUX
 #endif
 #ifdef _WIN32
     #define PLATFORM_WINDOWS
+#endif
+
 #endif
